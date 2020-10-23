632ee19a0e9d32d14a63e79c8db91540970bb69a
MagdalenaJasmine <magdalenacooper92@gmail.com>
2020-10-23 13:59:45 -0700

    added shop page component and shopping data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
+import ShopPage from "./components/shop/shop.component";
 import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
