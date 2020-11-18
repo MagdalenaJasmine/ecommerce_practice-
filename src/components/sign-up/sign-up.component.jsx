0d916137accc1598e1d5765bbe0e68543dd04111
MagdalenaJasmine <magdalenacooper92@gmail.com>
2020-11-17 18:00:39 -0800

    added sign-up form to page and connected with firestore

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./sign-up.styles.scss";
 
-import { auth, createUserProfileDocument } from "../firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
 import FormInput from "../../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
