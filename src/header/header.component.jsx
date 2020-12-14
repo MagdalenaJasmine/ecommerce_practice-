dbc59417d6009311d4fdcb271df3bf177b00a76f
MagdalenaJasmine <magdalenacooper92@gmail.com>
2020-12-14 15:09:37 -0800

    added cart dropdown to header and updated styling

--- src/header/header.component.jsx
+++ src/header/header.component.jsx
@@ -7,6 +7,7 @@ import { ReactComponent as Logo } from "./../assets/crown.svg";
 import { auth } from "../firebase/firebase.utils";
 import { connect } from "react-redux";
 import CartIcon from "../components/cart-icon/cart-icon.component";
+import CartDropdown from "../components/cart/cart-dropdown.component";
 
 const Header = ({ currentUser }) => (
   <div className="header">
@@ -31,6 +32,7 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 );
 
