16072ba4b0ccc7930814017c855d3e902d179e81
MagdalenaJasmine <magdalenacooper92@gmail.com>
2021-01-21 16:29:49 -0800

    updated total on checkout page so it reflects number of items in cart.

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,7 +1,8 @@
-export const addItemsToCart = (cartItems, cartItemToAdd) => {
+export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(
     (cartItem) => cartItem.id === cartItemToAdd.id
   );
+
   if (existingCartItem) {
     return cartItems.map((cartItem) =>
       cartItem.id === cartItemToAdd.id
@@ -21,9 +22,10 @@ export const removeItemFromCart = (cartItems, cartItemToRemove) => {
   if (existingCartItem.quantity === 1) {
     return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
   }
+
   return cartItems.map((cartItem) =>
     cartItem.id === cartItemToRemove.id
-      ? { ...cartItems, quantity: cartItem.quantity - 1 }
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
       : cartItem
   );
 };
