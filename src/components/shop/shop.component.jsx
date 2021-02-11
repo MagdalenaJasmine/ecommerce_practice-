140b82d14862877f8aab279ee47e6be8a9c8549a
MagdalenaJasmine <magdalenacooper92@gmail.com>
2021-02-10 17:05:18 -0800

    created collection overview component and moved collections from shop page to component.

--- src/components/shop/shop.component.jsx
+++ src/components/shop/shop.component.jsx
@@ -1,21 +1,10 @@
 import React from "react";
-import { connect } from "react-redux";
-
-import { createStructuredSelector } from "reselect";
-import CollectionPreview from "../preview-collection/preview-collection.component.jsx";
-
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import CollectionOverview from "../collection-overview/collection-overview.component";
 
 const ShopPage = ({ collections }) => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionOverview />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
